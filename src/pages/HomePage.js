1cc759b3e3d323661da61c24dad6306509a11afb
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2024-02-13 18:55:04 +0530

    carousel changed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel'
-import { SITE_SETTING } from '../env.conf';
-import HighLights from '../components/home/HighLights';
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { SITE_SETTING } from "../env.conf";
+// import HighLights from '../components/home/HighLights';
 
 class HomePage extends Component {
+  carousel_list = SITE_SETTING.dashboard_carousel;
 
-    carousel_list = SITE_SETTING.dashboard_carousel;
+  render() {
+    return (
+      <div>
+        <Carousel>
+          {this.carousel_list.map(
+            ({ title, caption, description, imageUrl, style }) => (
+              <Carousel.Item interval={4000} key={title}>
+                <img className="d-block w-100" src={imageUrl} alt={title} />
+                <Carousel.Caption style={style}>
+                  <h3>{caption}</h3>
+                  <p>{description}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          )}
+        </Carousel>
 
-    render() {
-        return (<div>
-            <Carousel>
-                {this.carousel_list.map(({ title, caption, description, imageUrl, style }) => (
-                    <Carousel.Item interval={1000} key={title} >
-                        <img
-                            className="d-block w-100"
-                            src={imageUrl}
-                            alt={title}
-                        />
-                        <Carousel.Caption style={style} >
-                            <h3>{caption}</h3>
-                            <p>{description}</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                ))
-                }
-            </Carousel>
-
-            <HighLights />
-        </div>);
-    }
+        {/* <HighLights /> */}
+      </div>
+    );
+  }
 }
 
 export default HomePage;
